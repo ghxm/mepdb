48daec2a1102681249978463959046484c8eda8f
ghxm <max.haag@gmx.net>
2020-10-21 17:18:52 +0200

    many improvements

--- bruteforce_mep_download.py
+++ bruteforce_mep_download.py
@@ -1,40 +1,111 @@
 # test and download MEP sites on the EU register by bruteforcing MEP IDs
 # @TODO: Logging
 # @TODO: Save to MongoDB and SQL
-# @TODO: Parallelization
-# @TODO: command-line arguments
 
-import re
 import urllib3
+import sqlite3
+import argparse
+import datetime
+import joblib
+import time
+
 import os
 import sys
-script_path = os.path.abspath(re.sub(r'(?<=mepdb\/).*', "", os.path.dirname(os.path.abspath(__file__))))
-sys.path.insert(0, os.path.abspath(script_path))
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+sys.path.insert(0, os.path.abspath(BASE_DIR))
 import utilities
+
+os.environ["TZ"] = "UTC"
+
+# set up logging
+log = utilities.log_to_file (os.path.join (BASE_DIR, 'logs/bruteforce_mep_download_%s.txt') % (
+    datetime.datetime.utcnow ().strftime ("%Y%m%d%H%M%S")), level = "INFO")
+
+# command line args
+parser = argparse.ArgumentParser ()
+parser.add_argument ("-p", "--parallel", action="store_true", default=False)
+parser.add_argument ("-v", "--verbose", action="count", default=0, help = "prints out iterations in parallel processing")
+parser.add_argument ("-n", "--njobs", default="auto")
+parser.add_argument ("-d", "--download", action="store_true", default=False) # Download MEP pages and save to MongoDB
+parser.add_argument ("--onlynew", action="store_true", default=False, help="automatically set lower ID range to highest ID in DB")
+parser.add_argument ("--lower", type = int, default=0)
+parser.add_argument ("--upper", type = int, default=999999)
+parser.add_argument ("-w", "--wait", type=int, help = "wait in seconds between requests", default=1)
+args = parser.parse_args()
+
+log.info (args)
+
+if args.parallel:
+    try:
+        args.njobs = int(args.njobs)
+    except:
+        if args.njobs == "auto":
+            args.njobs = int(joblib.cpu_count ())*10-5
+        else:
+            raise (Exception("No valid value for --njobs supplied"))
 
 config = utilities.get_config()
 
 # MongoDB connection
-utilities.connect_mongodb()
-mdb_db = utilities.connect_mongodb()
-mdb_col = mdb_db[config.get('MONGODB', '')]
+#utilities.connect_mongodb()
+#mdb_db = utilities.connect_mongodb()
+#mdb_col = mdb_db[config.get('MONGODB', '')]
+
+# SQL connection
+conn = utilities.connect_sqlite()
 
 headers = {
-    'user-agent': 'EUPLEX-MEPDB webspider (spiders@euplex.org)',
+    'user-agent': 'EUPLEX-MEPDB webspider (spiders@euplex.org)', # to identify the webspider vis a vis the server
     'accept-language': 'en-gb'}
 
-id_range = range(0,999999)
+if args.onlynew:
+    cur = conn.cursor()
+    db_mep_ids  = [i[0] for i in cur.execute('SELECT mep_id FROM meps').fetchall()]
+    db_mep_ids.sort()
+    if len(db_mep_ids) != 0:
+        args.lower = db_mep_ids[-1]
+
+
+id_range = range(args.lower, args.upper)
 
 http = urllib3.PoolManager()
 
-# save ID to mep.db
-for id in id_range:
-    url = re.sub(r'%id%', str(id), config.get('GENERAL').get('base_url'))
+def pipeline(id):
+    # save ID to mep.db
+    url = config.get('GENERAL', 'mep_register_base_url').format(id=str(id))
     # request
+    log.info('ID ' + str(id) + ': requesting ' + str(url))
+    timestamp = datetime.datetime.utcnow ()
     request = http.request('GET', url, redirect=5, retries=5, headers=headers)
 
+    log.info('ID ' + str(id) + ': Status ' + str(request.status))
+
     if 399 < int(request.status) < 500:
-        continue
-    else:
+        pass
+    elif int(request.status) == 200:
+        log.info('ID ' + str(id) + ': successful request')
         # save id to mep.db
+        try:
+            cur = conn.cursor()
+            log.info('ID ' + str(id) + ': adding to SQLITE DB...')
+            cur.execute('INSERT INTO meps (mep_id) VALUES (?);', (id,))
+            new = True
+        except sqlite3.IntegrityError:
+            log.info('ID ' + str(id) + ': already in SQLITE DB')
+            new = False
+
         # save HTML to MongoDB
+        if new and args.download:
+            # @TODO: add to MongoDB
+            pass
+    else:
+        log.info('ID ' + str(id) + ': unhandled status code' + str(request.status) + ", ID: " + str(id))
+
+    time.sleep(args.wait)
+
+if not args.parallel:
+    for id in id_range:
+        pipeline(id)
+else:
+    # run parallelized version
+    joblib.Parallel(require = "sharedmem", n_jobs=args.njobs, verbose=args.verbose)(joblib.delayed(pipeline)(id) for id in id_range)
--- utilities.py
+++ utilities.py
@@ -1,12 +1,28 @@
 # collection functions used across scripts
 
 import configparser
+import sqlite3
 import os
 import sys
-import re
+import pymongo
 import mongo_proxy
-script_path = os.path.abspath(re.sub(r'(?<=mepdb\/).*', "", os.path.dirname(os.path.abspath(__file__))))
-sys.path.insert(0, os.path.abspath(script_path))
+import logging
+
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+sys.path.insert(0, os.path.abspath(BASE_DIR))
+
+def log_to_file(filename, level=logging.INFO):
+    handler = logging.FileHandler (filename)
+    handler.setLevel (level)
+    formatter = logging.Formatter (fmt='%(asctime)s %(levelname)-8s %(message)s', datefmt='%Y-%m-%d %H:%M:%S')
+    handler.setFormatter (formatter)
+    loggername = filename
+    logger = logging.getLogger (loggername)
+    logger.setLevel (level)
+    logger.addHandler (handler)
+
+    return logger
+
 
 def get_config():
 
@@ -32,3 +48,35 @@ def connect_mongodb(connect = True):
     mdb_db = mdb_client[config.get ('MONGODB', 'db')]
 
     return mdb_db
+
+def connect_sqlite(check_same_thread=False):
+
+    config = get_config()
+
+    db_path = os.path.join(BASE_DIR, config.get ('SQL', 'sqlite_db'))
+
+    print(db_path)
+
+    if os.path.exists(db_path):
+        pass
+    else:
+        raise Exception("DB file does not exist!")
+
+
+    #if config.getboolean('SQL', 'authentication'):
+    #    psql_conn = sqlite3.connect (host=config.get ('PSQL', 'host'), database=config.get ('PSQL', 'db'),
+    #                              user=config.get ('PSQL', 'user'), password=config.get ('PSQL', 'password'), port=config.get('PSQL', 'port'))
+    #else:
+
+    conn = sqlite3.connect (database=db_path, check_same_thread=check_same_thread)
+
+    conn.isolation_level = None
+
+    return conn
+
+def add_mep_html_mongodb(html, mep_id, url, timestamp, replace, timediff, mdb_db):
+    # @TODO: if timediff large than x between last saved html
+    # @TODO: OR if change in HTML code and replace is False
+    # @TODO: store in MongoDB
+
+    pass
